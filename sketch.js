69c2196c7556d112c9075846c88d0176a4634a8e
rich123tyr <70693161+rich123tyr@users.noreply.github.com>
2020-09-16 20:44:18 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -21,12 +21,12 @@ function draw() {
     if(deformation<100){
       car.shapeColor=color(0,255,0);
     }
-    if(deformation>100 && deformation<180){
+    else if(deformation>100 && deformation<180){
       car.shapeColor=color(230,230,0);
     }
-    if(deformation>180){
+    else(deformation>180)
       car.shapeColor=color(255,0,0);
-    }
+    
 
   }
   drawSprites();
